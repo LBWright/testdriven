a067a9d9e24f48cbd662a1e6148c7ce4eaa9dab0
LBWright <lwright.js@gmail.com>
2019-06-27 20:39:33 -0500

    initial docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,20 +1,23 @@
+# services/users/project/__init__.py
+
+import sys
+import os
 from flask import Flask
 from flask_restful import Resource, Api
- 
+
 # initialize app
 app = Flask(__name__)
 api = Api(app)
 
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
+
 
 class UsersPing(Resource):
     def get(self):
-        return {
-                'status': 'success',
-                'message': 'pong!'
-        }
+        return {"status": "success", "message": "pong!"}
 
 
-api.add_resource(UsersPing, '/users/ping')
+api.add_resource(UsersPing, "/users/ping")
