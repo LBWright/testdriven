1f75859946a7e4a1d001afdc5ab9383befa5b110
LBWright <lwright.js@gmail.com>
2019-06-27 21:12:20 -0500

    adds postgres

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,9 @@
 # services/users/project/__init__.py
 
-import sys
 import os
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # initialize app
 app = Flask(__name__)
@@ -13,6 +13,21 @@ api = Api(app)
 # set config
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
+
+# instantiate db
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,28 @@
+import os
+
+
 class BaseConfig:
     """Base Configuration"""
+
     TESTING = False
+    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    pass
+
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
+
     TESTING = True
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
+
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    pass
+
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
